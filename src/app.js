ea0534b0b38bfb5f552f93125c17c64be2d049fe
TanmayGhosh347 <tanmay2690@gmail.com>
2024-06-24 09:48:22 +0530

    all set up configure

--- src/app.js
+++ src/app.js
@@ -13,5 +13,11 @@ app.use(cookieParser());
 
 
 
+// routes import 
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use('/api/v1/users' , userRouter);
+/*  http://localhost:8000/api/v1/users/register */
 
 export {app};
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
-const asyncHandler = (requestHandler) => async ( req , res , next ) =>  {
-  Promise.resolve(requestHandler(req , res , next )).catch((err) => next(err))
+const asyncHandler = (requestHandler) =>{
+  return  ( req , res , next ) =>  {
+    Promise.resolve(requestHandler(req , res , next )).catch((err) => next(err))
+  }
 }
 
 
